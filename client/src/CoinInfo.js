be6dc045c58f5f4bef1ac457a8844adcc2751de2
Darko Sancanin <darko.sancanin@gmail.com>
2019-09-17 21:07:48 +1000

    Playing around with time series graph.

--- client/src/CoinInfo.js
+++ client/src/CoinInfo.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { CoinContext } from "./CoinContext";
 import { Helmet } from "react-helmet";
 import { API_BASE_URL } from "./Api";
-// import { Line } from "@nivo/line";
+import { Line, ResponsiveLine } from "@nivo/line";
 import { ResponsivePieCanvas } from "@nivo/pie";
 
 const Cointainer = styled.div`
@@ -61,20 +61,6 @@ export const CoinInfo = props => {
     }
   }, [coinContext.symbol]);
 
-  const renderCustomPieChartLabel = e => {
-    return (
-      <text
-        x={e.x}
-        y={e.y}
-        fill="#ffffff"
-        textAnchor={e.x > e.cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {` ${e.value}% `}
-      </text>
-    );
-  };
-
   const formatDate = dateValue => {
     const months = [
       "Jan",
@@ -107,86 +93,91 @@ export const CoinInfo = props => {
     height: 350px;
   `;
 
+  const LineContainer = styled.div`
+    width: 700px;
+    height: 400px;
+  `;
+
   return (
     <Cointainer>
-      {/* <Line
-        width={900}
-        height={400}
-        animate={true}
-        margin={{ top: 20, right: 20, bottom: 60, left: 80 }}
-        data={[
-          {
-            id: "positive :)",
-            data: [
-              { x: 0, y: 0.7 },
-              { x: 1, y: 0.9 },
-              { x: 2, y: 0.8 },
-              { x: 3, y: 0.6 },
-              { x: 4, y: 0.3 },
-              { x: 5, y: 0 },
-              { x: 6, y: null },
-              { x: 7, y: null },
-              { x: 8, y: null },
-              { x: 9, y: null },
-              { x: 10, y: null },
-              { x: 11, y: 0 },
-              { x: 12, y: 0.4 },
-              { x: 13, y: 0.6 },
-              { x: 14, y: 0.5 },
-              { x: 15, y: 0.3 },
-              { x: 16, y: 0.4 },
-              { x: 17, y: 0 }
-            ]
-          },
-          {
-            id: "negative :(",
-            data: [
-              { x: 5, y: 0 },
-              { x: 6, y: -0.3 },
-              { x: 7, y: -0.5 },
-              { x: 8, y: -0.9 },
-              { x: 9, y: -0.2 },
-              { x: 10, y: -0.4 },
-              { x: 11, y: 0 },
-              { x: 12, y: null },
-              { x: 13, y: null },
-              { x: 14, y: null },
-              { x: 15, y: null },
-              { x: 16, y: null },
-              { x: 17, y: 0 },
-              { x: 18, y: -0.4 },
-              { x: 19, y: -0.2 },
-              { x: 20, y: -0.1 },
-              { x: 21, y: -0.6 }
-            ]
-          }
-        ]}
-        curve="monotoneX"
-        enablePointLabel={true}
-        pointSize={4}
-        pointBorderWidth={1}
-        pointBorderColor={{
-          from: "color",
-          modifiers: [["darker", 0.3]]
-        }}
-        pointLabelYOffset={-20}
-        enableGridX={false}
-        colors={["rgb(97, 205, 187)", "rgb(244, 117, 96)"]}
-        xScale={{
-          type: "linear"
-        }}
-        yScale={{
-          type: "linear",
-          stacked: false,
-          min: -1,
-          max: 1
-        }}
-        enableArea={true}
-        areaOpacity={0.14}
-        enableSlices={false}
-        useMesh={true}
-        crosshairType="cross"
-      /> */}
+      <LineContainer>
+        <ResponsiveLine
+          animate={true}
+          margin={{ top: 20, right: 20, bottom: 60, left: 80 }}
+          data={[
+            {
+              id: "positive :)",
+              data: [
+                { x: 0, y: 0.7 },
+                { x: 1, y: 0.9 },
+                { x: 2, y: 0.8 },
+                { x: 3, y: 0.6 },
+                { x: 4, y: 0.3 },
+                { x: 5, y: 0 },
+                { x: 6, y: null },
+                { x: 7, y: null },
+                { x: 8, y: null },
+                { x: 9, y: null },
+                { x: 10, y: null },
+                { x: 11, y: 0 },
+                { x: 12, y: 0.4 },
+                { x: 13, y: 0.6 },
+                { x: 14, y: 0.5 },
+                { x: 15, y: 0.3 },
+                { x: 16, y: 0.4 },
+                { x: 17, y: 0 }
+              ]
+            },
+            {
+              id: "negative :(",
+              data: [
+                { x: 5, y: 0 },
+                { x: 6, y: -0.3 },
+                { x: 7, y: -0.5 },
+                { x: 8, y: -0.9 },
+                { x: 9, y: -0.2 },
+                { x: 10, y: -0.4 },
+                { x: 11, y: 0 },
+                { x: 12, y: null },
+                { x: 13, y: null },
+                { x: 14, y: null },
+                { x: 15, y: null },
+                { x: 16, y: null },
+                { x: 17, y: 0 },
+                { x: 18, y: -0.4 },
+                { x: 19, y: -0.2 },
+                { x: 20, y: -0.1 },
+                { x: 21, y: -0.6 }
+              ]
+            }
+          ]}
+          curve="monotoneX"
+          enablePointLabel={true}
+          pointSize={4}
+          pointBorderWidth={1}
+          pointBorderColor={{
+            from: "color",
+            modifiers: [["darker", 0.3]]
+          }}
+          pointLabelYOffset={-20}
+          enableGridX={false}
+          colors={["rgb(97, 205, 187)", "rgb(244, 117, 96)"]}
+          xScale={{
+            type: "linear"
+          }}
+          yScale={{
+            type: "linear",
+            stacked: false,
+            min: -1,
+            max: 1
+          }}
+          enableArea={true}
+          areaOpacity={0.14}
+          enableSlices={false}
+          useMesh={true}
+          crosshairType="cross"
+        />
+      </LineContainer>
       {hasError && <Error>Oops something went wrong. Please try again.</Error>}
       {isLoading && <img src="/loading.gif" alt="Loading..." />}
       {coin && (
@@ -222,7 +213,7 @@ export const CoinInfo = props => {
                 padAngle={2.5}
                 cornerRadius={3}
                 colors={["#e8d73b", "#e8d73b0F"]}
-                borderWidth="1"
+                borderWidth={1}
                 borderColor={"#FFFFFF"}
                 radialLabelsSkipAngle={10}
                 radialLabelsTextXOffset={6}
@@ -233,7 +224,6 @@ export const CoinInfo = props => {
                 radialLabelsLinkStrokeWidth={1}
                 radialLabelsLinkColor={{ from: "color" }}
                 sliceLabel={d => `${d.value}%`}
-                tooltipFormat={d => `${d.value}%`}
                 slicesLabelsSkipAngle={10}
                 slicesLabelsTextColor="#FFFFFF"
                 animate={true}
